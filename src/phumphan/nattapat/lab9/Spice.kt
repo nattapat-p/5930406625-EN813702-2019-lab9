af0a28aee39d5576628b9339dc46afd42e96d7a1
42835308 <nattapat.p@outlook.co.th>
2020-02-19 14:54:19 +0700

    Initial commit

---
+++ src/phumphan/nattapat/lab9/Spice.kt
@@ -0,0 +1,44 @@
+package phumphan.nattapat.lab9
+
+fun main(){
+    var spicies = listOf(Spice("green curry"),
+        Spice("red curry","medium"),
+        Spice("pepper","spicy"),
+        Spice("green pepper","very spicy")
+        )
+
+    val spicies2 = getSpicyOrLess(spicies)
+
+    println("=== Printing only spicy or less than spicy ===")
+    for(s2 in spicies2)
+        println("${s2.name} has spicy level as ${s2.spiciness}")
+
+
+    println("=== Make Salt ===")
+   makeSalt()
+}
+
+class Spice (val name: String , val spiciness: String = "mild"){
+    val heat: Int
+    get() {
+        return when(spiciness){
+            "mild" -> 1
+            "medium"->3
+            "spicy"->5
+            "very spicy" -> 7
+            else -> 10
+        }
+    }
+
+    init {
+        println("spice ${this.name} has spicy level as ${this.spiciness}")
+    }
+}
+
+fun getSpicyOrLess(list:List<Spice> ): List<Spice> {
+return list.filter { it.heat <= 5 }
+}
+
+ fun makeSalt(){
+    Spice("salt")
+}
